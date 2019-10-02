5700c77f61fe02a9c09e6ce0f84d14f53b3e85dc
Giulio Marin <giulio.marin@me.com>
2019-10-01 22:50:10 -0700

    wip use deps from other repo

--- cmake/FindFreeImage.cmake
+++
@@ -1,9 +0,0 @@
-set(deps_path ${CMAKE_CURRENT_SOURCE_DIR}/../deps)
-
-set(FreeImage_LIBRARIES "${deps_path}/freeimage/macos/lib64/libfreeimage.a")
-set(FreeImage_LIBRARY ${FreeImage_LIBRARIES})
-
-set(FreeImage_INCLUDE_DIRS "${deps_path}/freeimage/macos/include")
-set(FreeImage_INCLUDE_DIR ${FreeImage_INCLUDE_DIRS})
-
-set(FreeImage_FOUND True)
--- cmake/Findglm.cmake
+++
@@ -1,9 +0,0 @@
-set(deps_path ${CMAKE_CURRENT_SOURCE_DIR}/../deps)
-
-set(glm_LIBRARIES "")
-set(glm_LIBRARY ${glm_LIBRARIES})
-
-set(glm_INCLUDE_DIRS "${deps_path}/glm-0.9.2.7")
-set(glm_INCLUDE_DIR ${glm_INCLUDE_DIRS})
-
-set(glm_FOUND True)
